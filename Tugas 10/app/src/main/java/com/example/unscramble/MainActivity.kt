d7cc973a28c58e3d66d089dabdea582a9eb61397
unknown <zienzidan41@gmail.com>
2025-05-28 17:19:36 +0700

    Tugas 10

---
+++ Tugas 10/app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.unscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.unscramble.ui.GameScreen
+import com.example.unscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnscrambleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    GameScreen()
+                }
+            }
+        }
+    }
+}
